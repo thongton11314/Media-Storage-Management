99183c69931dd3ab09c9a4f05d5281a1c3b5a289
Vincent Le <60553826+mincedvince@users.noreply.github.com>
2020-12-02 16:55:09 -0800

    Update command_inventory.cpp

--- command_inventory.cpp
+++ command_inventory.cpp
@@ -1,17 +1,20 @@
 #include "command_inventory.h"
-
+//----------------------------------------------------------------------------
+// Constructor
 Inventory::Inventory() {
     this->type = INVENTORY;
 }
-
+//----------------------------------------------------------------------------
+// Destructor
 Inventory::~Inventory() {
 }
-
+//----------------------------------------------------------------------------
+//processInventory
 void Inventory::processInventory(MediaCollection & meColl) {
     cout << "Inventory:" << endl;
     meColl.display();
 }
-
+//----------------------------------------------------------------------------
 ostream& operator<<(ostream & out, const Inventory & command) {
     out << command.type;
     return out;
