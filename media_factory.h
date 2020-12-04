6257a3abdb526b65b345d2ef47691ce1114045c2
CoderYud <73090278+CoderYud@users.noreply.github.com>
2020-12-03 23:27:22 -0800

    Update media_factory.h

--- media_factory.h
+++ media_factory.h
@@ -2,6 +2,12 @@
 #define _MEDIA__FACTORY_
 #include "media_movie_factory.h"
 
+// Class MediaFactory:
+//  Description:
+//   - This factory create DVD type movie
+//  Implementation:
+//   - Create Hashtable:
+//     - Create DVD type movie for the system.
 class MediaFactory {
 public:
     static Media * createDVDMovie(ifstream & infile);
