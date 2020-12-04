4e60245cbb7b4fd123d8feb92bd6fd1324b9c4b2
CoderYud <73090278+CoderYud@users.noreply.github.com>
2020-12-04 00:31:06 -0800

    Update media.cpp

--- media.cpp
+++ media.cpp
@@ -1,19 +1,34 @@
 #include "media.h"
 
+//----------------------------------------------------------------------------
+// Media
+// Destructor
 Media::~Media() {}
 
+//----------------------------------------------------------------------------
+// setStock
+// Modify the number of stock in the system
 void Media::setStock(unsigned int amount) {
     this->stock = amount;
 }
 	
+//----------------------------------------------------------------------------
+// getStock
+// Return the amount of stock in the system
 int Media::getStock() const {
     return this->stock;
 }
 
+//----------------------------------------------------------------------------
+// addStock
+// Increase the number of stock in the system
 void Media::addStock(unsigned int amount) {
     this->stock += amount;
 }
 
+//----------------------------------------------------------------------------
+// reduceStock
+// Reduce the amount of stock in the system
 bool Media::reduceStock(int amount) {
     if (this->stock - amount < 0)
         return false;
@@ -22,6 +37,9 @@ bool Media::reduceStock(int amount) {
     return true;
 }
 
+//----------------------------------------------------------------------------
+// operator<<
+// Print out the media to the system
 ostream& operator<<(ostream & out, const Media & media) {
     media.print(out);
     return out;
